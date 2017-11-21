ce33fbb4f49f79bf3c28bc34111772317493732e
z4zr <z4zr@qq.com>
2017-11-21 22:19:51 +0800

    修正

--- chapter12 Java模块化/java9-module-starter/run.sh
+++ chapter12 Java模块化/java9-module-starter/run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir out
+
 javac -d out --module-source-path src --module dockerx.module.starter
 
-mdkdir out
+mkdir mods
 
 jar --create --file mods/dockerx.module.starter.jar --main-class com.example.Main --module-version 1.0 -C out/dockerx.module.starter .
-
-mdkdir mods
 
 java --module-path mods --module dockerx.module.starter
 
