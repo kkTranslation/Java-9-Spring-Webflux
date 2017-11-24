b38023662911a0628a8c700758ff3d118276c317
z4zr <z4zr@qq.com>
2017-11-25 00:58:57 +0800

    调整例子dockerx-traffic-optional-gui和例子dockerx-traffic-optional-gui-pro

--- chapter12 Java模块化/java9-action-static-pro/dockerx-traffic-optional-gui/src/main/java/com/dockerx/traffic/optional/gui/Gui.java
+++ chapter12 Java模块化/java9-action-static-pro/dockerx-traffic-optional-gui/src/main/java/com/dockerx/traffic/optional/gui/Gui.java
@@ -10,17 +10,20 @@ import com.dockerx.traffic.optional.three.Student;
  */
 public class Gui {
     public static void main(String[] args) {
-        //module-info.java 文件中使用static修饰后,会在编译阶段直接忽略这个类，不会加载到文件系统中，
-        //而当其他依赖模块中比如(dockerx.traffic.optional.three模块)中存在此模块所忽略的模块，
-        //那么，Jrtfs依然会加载到其文件系统中，于是就可以在运行时正常使用此module-info文件中所忽略的模块中的类了
+        // module-info.java 文件中使用static修饰后,会在编译阶段直接忽略这个类，不会加载到文件系统中，
+        // 而当其他依赖模块中比如(dockerx.traffic.optional.three模块)中存在此模块所忽略的模块，
+        // 那么，Jrtfs依然会加载到其文件系统中，于是就可以在运行时正常使用此module-info文件中所忽略的模块中的类了
         Person person = new Person(100, 1);
         System.out.println(Person.class.getAnnotations().length);
 
-        System.out.println(Bike.class.getAnnotations().length);
-        System.out.println("Running with annotation @Anno present.");
+        if (Bike.class.getAnnotations().length > 0) {
+            System.out.println("Running with annotation @Anno present.");
+        } else {
+            System.out.println("Running without annotation @Anno present.");
+        }
 
         Student student = new Student();
-        //同样，使用反射也是可以的
+        // 同样，使用反射也是可以的
         try {
             Class<?> clazz = Class.forName("com.dockerx.traffic.optional.entity.Person");
             Person instance =
--- chapter12 Java模块化/java9-action-static/dockerx-traffic-optional-gui/src/main/java/com/dockerx/traffic/optional/gui/Gui.java
+++ chapter12 Java模块化/java9-action-static/dockerx-traffic-optional-gui/src/main/java/com/dockerx/traffic/optional/gui/Gui.java
@@ -2,6 +2,8 @@ package com.dockerx.traffic.optional.gui;
 
 import com.dockerx.traffic.optional.entity.Person;
 
+import java.lang.annotation.Annotation;
+
 /**
  * @author Author  知秋
  * @email fei6751803@163.com
@@ -9,14 +11,17 @@ import com.dockerx.traffic.optional.entity.Person;
  */
 public class Gui {
     public static void main(String[] args) {
-        //module-info.java 文件中使用static修饰后,会在编译阶段直接忽略这个类，不会加载到文件系统中，
+        // module-info.java 文件中使用static修饰后,会在编译阶段直接忽略这个类，不会加载到文件系统中，
         // 下面注释掉的两条语句都会报ClassNotFoundException
-        /*Person person = new Person(100, 1);
-        System.out.println(Person.class.getAnnotations().length);*/
+        // Person person = new Person(100, 1);
+        // System.out.println(Person.class.getAnnotations().length);
 
-        System.out.println(Bike.class.getAnnotations().length);
-        System.out.println("Running without annotation @Anno present.");
-        //同样，使用反射也是不行的，会报下面catch到的错了
+        if (Bike.class.getAnnotations().length > 0) {
+            System.out.println("Running with annotation @Anno present.");
+        } else {
+            System.out.println("Running without annotation @Anno present.");
+        }
+        // 同样，使用反射也是不行的，会报下面catch到的错了
         try {
             Class<?> clazz = Class.forName("com.dockerx.traffic.optional.entity.Person");
             Person instance =
